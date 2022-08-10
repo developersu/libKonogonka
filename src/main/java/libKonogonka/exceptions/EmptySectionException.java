55fb58f25bf632264aa650f2d3756d155d3f4c6a
Dmitry Isaenko <developer.su@gmail.com>
2022-08-10 20:20:10 +0300

    Update copyrights

--- src/main/java/libKonogonka/exceptions/EmptySectionException.java
+++ src/main/java/libKonogonka/exceptions/EmptySectionException.java
@@ -1,3 +1,22 @@
+/*
+    Copyright 2019-2022 Dmitry Isaenko
+
+    This file is part of libKonogonka.
+
+    libKonogonka is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    libKonogonka is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with libKonogonka.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 package libKonogonka.exceptions;
 
 public class EmptySectionException extends NullPointerException {
